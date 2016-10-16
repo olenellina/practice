ec35455573438902cb2539ed4bcad11b7dda7a5a
Melissa Rodriguez <olenellina@gmail.com>
2016-10-15 23:48:43 -0700

    solution for arraysrrays 5 now working with one hash

--- arrays_5.rb
+++ arrays_5.rb
@@ -1,25 +1,23 @@
 # Given two arrays of integers, determine if they are the same other than order.
+# Bonus: do this with one hash
 
 def equal(array1, array2)
-  hash1 = {}
-  hash2 = {}
+  hash = {}
   array1.length.times do |x|
-    if hash1.has_key?(array1[x])
-      hash1[array1[x]] += 1
+    if hash.has_key?(array1[x])
+      hash[array1[x]] += 1
     else
-      hash1[array1[x]] = 0
+      hash[array1[x]] = 0
     end
   end
   array2.length.times do |x|
-    if hash2.has_key?(array2[x])
-      hash2[array2[x]] += 1
+    if hash.has_key?(array2[x])
+      hash[array2[x]] += 1
     else
-      hash2[array2[x]] = 0
+      return false
     end
   end
-  print hash1
-  print hash2
-  return hash1 == hash2
+  return true
 end
 
-print equal([1,2,3,4,5], [5,4,3,2,1,1])
+print equal([1,2,3,4,5], [5,4,3,2,1,1,6])
