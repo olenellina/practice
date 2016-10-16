d4c6fea48e937fa3976bca13675918fd8a5ff2f5
Melissa Rodriguez <olenellina@gmail.com>
2016-10-16 15:46:01 -0700

    better solution for arrays #5 (bonus)

--- arrays_5.rb
+++ arrays_5.rb
@@ -2,18 +2,49 @@
 # Bonus: do this with one hash
 
 def equal(array1, array2)
-  hash = {}
+  hash1 = {}
+  hash2 = {}
   array1.length.times do |x|
-    if !hash.has_key?(array1[x])
-      hash[array1[x]] = 0
+    if hash1.has_key?(array1[x])
+      hash1[array1[x]] += 1
+    else
+      hash1[array1[x]] = 1
     end
   end
   array2.length.times do |x|
-    if !hash.has_key?(array2[x])
+    if hash2.has_key?(array2[x])
+      hash2[array2[x]] += 1
+    else
+      hash2[array2[x]] = 1
+    end
+  end
+  return hash1 == hash2
+end
+
+print equal([1,2,3,4,5], [5,4,3,2,1,1])
+
+
+def equal(array1, array2)
+  hash = {}
+  array1.length.times do |x|
+    if hash.has_key?(array1[x])
+      hash[array1[x]] += 1
+    else
+      hash[array1[x]] = 1
+    end
+  end
+
+  array2.length.times do |x|
+    if hash.has_key?(array2[x])
+        hash[array2[x]] -= 1
+          if hash[array2[x]] == -1
+            return false
+          end
+    else
       return false
     end
   end
   return true
 end
 
-print equal([1,2,3,4,5], [5,4,3,2,1,1])
+print equal([1,2,3,4,5], [5,4,3,2,1])
