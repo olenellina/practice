93c0fb5d9a44e0e6ac0b2a228b56cb2b49b8467b
Melissa Rodriguez <olenellina@gmail.com>
2016-10-15 17:35:33 -0700

    working solution for problem #11

---
+++ practice.rb
@@ -0,0 +1,15 @@
+# Consider an array representing a compressed string, where the array stores a series of string and repeat values. For example, a = [‘ab’, 2] represents “abab”, and a = [‘a’, 2, ‘b’, 3] represents “aabbb”. Given such an array, print the string it represents.
+
+def compressed(array)
+  x = 0
+  string = ""
+  array.length.times do
+    array[x+1].to_i.times do
+      string = string + array[x]
+    end
+    x += 2
+  end
+  return string
+end
+
+print compressed(["ab", 2, "bb", 3])
