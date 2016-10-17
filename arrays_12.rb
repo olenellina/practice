b3e051743107577013a0ea1aadef476c7ab48b0a
Melissa Rodriguez <olenellina@gmail.com>
2016-10-17 11:56:21 -0700

    still trying to get arrays #12 working

--- arrays_12.rb
+++ arrays_12.rb
@@ -2,25 +2,20 @@
 
 def compress(string)
   array = []
-  x = 0
-  y = string.length
-  y.times do
-    counter = 1
-    if string[x] != string[x-1]
-      char = string[x]
-      if char == string[x+1]
-        while string[x] == string[x+1]
-          counter += 1
-          x += 1
-        end
-      else
+  string.length.times do |x|
+    counter = 0
+    char = string[x]
+    if char == string[x + 1]
+      while char == string[x+1]
+        counter += 1
         x += 1
       end
-      array.push(char)
-      array.push(counter)
     else
-      x += 1
+      x +=1
+      counter += 1
     end
+    array.push(char)
+    array.push(counter)
   end
   print array
 end
