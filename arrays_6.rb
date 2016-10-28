d00f906e1bd4f939182ce19c512c5d7235439161
Melissa Rodriguez <olenellina@gmail.com>
2016-10-27 21:14:39 -0700

    actually pushign csaving changes

--- arrays_6.rb
+++ arrays_6.rb
@@ -1,11 +1,12 @@
 ########## No Additional Storage ###############
 
-def equal(array1, array2)
-	if array1.sort == array2.sort
+def sorted(array)
+	if array.sort == array
 		return true
 	else
 		return false
 	end
 end
 
-print equal([5,1,2,3,4,5], [5,4,3,2,1,5])
+print sorted([5,1,2,3,4,5])
+print sorted([1,2,3,4,5,6])
